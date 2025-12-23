b6bfaa8999ab702316d93cb0cfb15722d86d1ccd
rachel-gillespie <gillespierachel74@gmail.com>
2025-12-23 23:16:42 +0000

    Add city card component and update site styling
    Introduced a new city-card-component.njk and removed the old city-focus-component.njk. Updated layout, header, nav, and footer for improved styling and modal support. Added new weather images and updated city pages to use the new card component and refreshed design. Also configured Eleventy to passthrough the js directory.

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,6 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
 };
